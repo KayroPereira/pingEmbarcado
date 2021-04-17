d1ae6ccc8773318da25f74e5cb69fd9a5e4812a7
Kayro Pereira <kayrofellyxbr@gmail.com>
2021-04-17 20:52:21 -0300

    Implementado ping com a lib Pinger.h

--- pingEmbarcado.cpp
+++ pingEmbarcado.cpp
@@ -1,13 +1,80 @@
 #include "pingEmbarcado.h"
 
-unsigned long 	sendPingDelay = 0,
-				realTime = 0;
+#define DELAY_PING_REPEAT	10000
 
-bool sendDataFirebase = false;
+const String pathPing[] = {"www.google.com", "undefinedname"};
 
+Pinger pinger;
 
-uint16_t count = 0;
+uint8_t control = 0;
+long timeReference = 0;
+bool repeatPing = true;
 
+void onReceivePing(){
+
+  pinger.OnReceive([](const PingerResponse& response){
+	if (response.ReceivedResponse){
+	  Serial.printf("Reply from %s: bytes=%d time=%lums TTL=%d\n",
+		response.DestIPAddress.toString().c_str(),
+		response.EchoMessageSize - sizeof(struct icmp_echo_hdr),
+		response.ResponseTime,
+		response.TimeToLive);
+	}
+	else{
+	  Serial.printf("Request timed out.\n");
+	}
+
+	// Return true to continue the ping sequence.
+	// If current event returns false, the ping sequence is interrupted.
+	return true;
+  });
+}
+
+void onEndPing(){
+	pinger.OnEnd([](const PingerResponse& response){
+		// Evaluate lost packet percentage
+		float loss = 100;
+		if(response.TotalReceivedResponses > 0){
+		  loss = (response.TotalSentRequests - response.TotalReceivedResponses) * 100 / response.TotalSentRequests;
+		}
+
+		// Print packet trip data
+		Serial.printf("Ping statistics for %s:\n",
+		  response.DestIPAddress.toString().c_str());
+		Serial.printf("    Packets: Sent = %lu, Received = %lu, Lost = %lu (%.2f%% loss),\n",
+		  response.TotalSentRequests,
+		  response.TotalReceivedResponses,
+		  response.TotalSentRequests - response.TotalReceivedResponses,
+		  loss);
+
+		// Print time information
+		if(response.TotalReceivedResponses > 0){
+		  Serial.printf("Approximate round trip times in milli-seconds:\n");
+		  Serial.printf("    Minimum = %lums, Maximum = %lums, Average = %.2fms\n",
+			response.MinResponseTime,
+			response.MaxResponseTime,
+			response.AvgResponseTime);
+		}
+
+		// Print host data
+		Serial.printf("Destination host data:\n");
+		Serial.printf("    IP address: %s\n",
+		  response.DestIPAddress.toString().c_str());
+
+		if(response.DestMacAddress != nullptr){
+		  Serial.printf("    MAC address: " MACSTR "\n",
+			MAC2STR(response.DestMacAddress->addr));
+		}
+
+		if(response.DestHostname != ""){
+		  Serial.printf("    DNS name: %s\n",
+			response.DestHostname.c_str());
+		}
+
+		repeatPing = true;
+		return true;
+	});
+}
 
 void setup() {
 
@@ -15,8 +82,9 @@ void setup() {
 
 	pinMode(LED_2, OUTPUT);
 
+	WiFi.mode(WIFI_STA);
 	WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
-	Serial.print("Connecting to Wi-Fi");
+	Serial.printf("\n\nConnecting to Wi-Fi");
 	while (WiFi.status() != WL_CONNECTED) {
 		Serial.print(".");
 		delay(300);
@@ -26,6 +94,9 @@ void setup() {
 	Serial.println(WiFi.localIP());
 	Serial.println();
 	WiFi.setAutoReconnect(true);
+
+	onReceivePing();
+	onEndPing();
 }
 
 void updatePin(uint8_t pin, uint8_t val){
@@ -34,13 +105,56 @@ void updatePin(uint8_t pin, uint8_t val){
 
 void loop() {
 
-	realTime = millis();
-
-	if (realTime - sendPingDelay > 15000) {
-
+	if(millis() - timeReference > DELAY_PING_REPEAT){
 
 		digitalWrite(LED_2, !digitalRead(LED_2));
 
-		sendPingDelay = millis();
+		timeReference = millis();
+		if(repeatPing){
+
+			repeatPing = false;
+
+			Serial.printf("\n\nControl = %d", control);
+			switch(control++){
+			case 0:
+			case 1:
+				// Ping path array
+				Serial.printf("\n\n%s\n", pathPing[control-1].c_str());
+				if(pinger.Ping(pathPing[control-1].c_str()) == false){
+					Serial.println("Error during ping command.");
+					repeatPing = true;
+				}
+				break;
+
+			case 2:
+				// Ping default gateway
+				Serial.printf("\n\nPinging default gateway with IP %s\n", WiFi.gatewayIP().toString().c_str());
+				if(pinger.Ping(WiFi.gatewayIP()) == false){
+					Serial.println("Error during last ping command.");
+					repeatPing = true;
+				}
+				break;
+
+			case 3:
+				  // Ping invalid ip
+				  Serial.printf("\n\nPinging invalid ip 1.2.3.4\n");
+				  if(pinger.Ping(IPAddress(1,2,3,4)) == false){
+					Serial.println("Error during ping command.");
+					repeatPing = true;
+				  }
+				break;
+
+			default:
+				Serial.printf("\n\n%s\n", pathPing[0].c_str());
+				if(pinger.Ping((pathPing[0].c_str())) == false){
+					Serial.println("Error during ping command.");
+					repeatPing = true;
+				}
+
+				if(control >= 5){
+					control = 0;
+				}
+			}
+		}
 	}
 }
--- pingEmbarcado.h
+++ pingEmbarcado.h
@@ -1,10 +1,15 @@
 
 #ifndef _testEspFirebaseCallback_H_
 #define _testEspFirebaseCallback_H_
-	#include "Arduino.h"
+//	#include "Arduino.h"
 
+	#include <Pinger.h>
 	#include <ESP8266WiFi.h>
 	#include "security.h"
+
+	extern "C"{
+	  #include <lwip/icmp.h> // needed for icmp packet definitions
+	}
 
 	#define LED_2			2			//GPIO2	-	D4
 
